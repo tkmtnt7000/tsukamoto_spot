52a03f6c5bddb328f63d0f704958228a02af3cc5
tkmtnt7000 <naototukka0413@gmail.com>
2021-04-25 19:54:21 +0900

    add angular part

--- scripts/joy_sub.py
+++ scripts/joy_sub.py
@@ -4,6 +4,8 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
+mode_flag = True
+
 def callback(msg):
     rospy.loginfo("publish cmd_vel!")
     cmd_vel_pub(msg)
@@ -14,10 +16,15 @@ def joy_sub():
     rospy.spin()
 
 def cmd_vel_pub(msg):
+    if msg.buttons[0] = 1:
+        mode_flag = not mode_flag
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     joy = Twist()
-    joy.linear.x = msg.axes[1]
-    joy.linear.y = msg.axes[0]
+    if mode_flag = True:
+        joy.linear.x = msg.axes[1]
+        joy.linear.y = msg.axes[0]
+    else:
+        joy.angular.z = msg.axes[0]
     pub.publish(joy)
 
 if __name__ =='__main__':
