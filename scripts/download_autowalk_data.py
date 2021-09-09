71696c5da7197842407077b9d38d6de14fb91067
Naoto Tsukamoto <naototukka0413@gmail.com>
2021-09-09 21:47:52 +0900

    update download_autowalk_data.py

---
+++ scripts/download_autowalk_data.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+from jsk_data import download_data
+
+
+def main():
+    PKG = 'spot_autowalk_data'
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_73B2_to_81C1.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1IDTCP7n4LCowizW3mFQvTOQtE4qOH9tx',
+        md5='65f09629c0ac2aa21df6f179c9875bd0',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_73B2_to_7FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1O8o6voq2v8WenfaUYcmpSU-IwJSsXW5_',
+        md5='ecd4d8dc043995f7675a59fce950676b',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_7FElevator_to_2FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=12MOg5okckmQlYiM6flkdeMYxqjn9-C9l',
+        md5='67ae3210cbfb55791fff6494f84abb3b',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_elevator_7FElevator_to_2FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1iyx0y1dPu4HUPMNepR_VaZd_WEaC5Lku',
+        md5='915916d084abd54c2c17f0738a726da3',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_2FElevator_to_2FEntrance.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1cYUn_qnRslWuH0ZMEBN6ovqmdcUB0MzY',
+        md5='78c6e1e8e5967b216c9f53e38893750e',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_2FElevator_to_MainGate.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1CIuStpjxIA188MLxUsfUjI-47Ev36Wiv',
+        md5='47d669bcb1394b97c95e5d77f78da3e5',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_2FElevator_to_eng8_2FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1oyU1ufqy9gryPXw8YZ45Ff7AR8K825dT',
+        md5='ac9a67567104df2ddd78b8c53fa61ba2',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng_TelephoneBox_to_HongoMainGate.walk.tar.gz',
+        url='https://drive.google.com/uc?id=120WC6SE4C_9XIvy0j3HEraljIigjcJ5U',
+        md5='6f89cd74bea3934171e0ae720747da24',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_3FElevator_to_Mech_Office.walk.tar.gz',
+        url='https://drive.google.com/uc?id=10pWP1qnbr5TCfQVUeOHnDacdwQ3X3Awh',
+        md5='52564afd6471c9551c8f77ac031a366d',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_elevator_3FElevator_to_2FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1OFB38ISYjTtu9A87QtfYyVLhtcGnpu2a',
+        md5='e8ed341b22c5eb66373d29b580bede05',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_2FElevator_to_MainEntrance.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1e5hCLtfOlDYWhrQoefYGxz2AnjaSfs_f',
+        md5='e79f25769b702c203d4f9229c81a1f89',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_MainEntrance_to_HongoMainGate.walk.tar.gz',
+        url='https://drive.google.com/uc?id=13mG7iFMkFWN04S7BeR2rPwvpL-0cTgko',
+        md5='e0260203d5a3fa4958014b4149c3749a',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_MainEntrance_to_HongoMainGate_Daylight.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1EHpKaMBa8rnRZRyK8KwTK3727cxNqph4',
+        md5='43cff60ed9dc444d3d7de0e4ef304d0f',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/73b2_inside.walk.tar.gz',
+        url='https://drive.google.com/uc?id=14BPDOJBNS2v5EoUyIZ9gDwm4VFXyj-FD',
+        md5='5b45fcd7271f42f32414602145b77bd8',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_1FElevator_to_1FGarbageCollectionAreaSouth.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1eZbBLwzl9nVpSAebtux5nbN1B4UfGEoh',
+        md5='01ef288775d6c21a634a17fa427e4900',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_1FElevator_to_1FGarbageCollectionAreaEast.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1_zySu-e94wzHLaLYTbQuUd9A0Sb8x9NQ',
+        md5='fc9fb3161b745ff5f7b62be8977f7e56',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_elevator_7FElevator_to_1FElevator.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1kLSNCn2lhkcdOFBaYSVHUFzklhVz_jL8',
+        md5='fa5a26677d9136ef4671d7a32ca59097',
+        extract=True
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='autowalk/eng2_1FElevator_to_1FGarbageCollectionAreaSouth_via213.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1oSFjkwG0EcVXapMVgLlc_i1VtcWXwEwE',
+        md5='72ba498fe56700bee31c8ce166e77008',
+        extract=True
+    )
+
+if __name__ == '__main__':
+    main()
